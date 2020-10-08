3dea6ff353ea8be68d09dd69806286f55e2d36bc
Tomasz Bembnista <t.bembnista@evorsorge.de>
2020-10-08 09:34:08 +0200

    #1 Try to render list of spaces

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { Grommet, Card, CardHeader, CardBody } from 'grommet';
 import { SpaceResourceApi } from "./app/srvapi";
 import { SpaceDTO } from "./app/srvapi/models";
-import { AxiosResponse } from "axios";
 
 function App() {
-  let spa : SpaceResourceApi = new SpaceResourceApi();
+  let spacesResource: SpaceResourceApi = new SpaceResourceApi();
 
-  spa.getSpacesUsingGET().then(spaces => {
-    
+  var spaces: SpaceDTO[] = [];
+
+  spacesResource.getSpacesUsingGET().then(spacesResult => {
+    spaces = spacesResult.data;
+    console.log("Spaces got! Count: " + spaces.length);
   })
-  
+
   return (
     <Grommet>
       <Card width="small" background="light-1">
         <CardHeader pad="medium">Spaces</CardHeader>
         <CardBody>
-
+          {spaces.map(space => (
+            <div key={space.id}>id: {space.id}, name: {space.name}</div>
+          ))}
         </CardBody>
       </Card>
     </Grommet>

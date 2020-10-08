6828ac790cce9cc7194339598035bf328faff198
Piotr Baranowski <piotr267@protonmail.com>
2020-10-08 14:32:27 +0200

    It is possible to navigate through spaces

--- src/app/ListSpaces.tsx
+++ src/app/ListSpaces.tsx
@@ -2,31 +2,41 @@ import React from "react";
 import { SpaceResourceApi } from "./srvapi";
 import { SpaceDTO } from "./srvapi/models/space-dto";
 
-export default class ListSpaces extends React.Component<any, ListSpacesState> {
-    constructor(props: any) {
+export default class ListSpaces extends React.Component<ListSpacesProps, ListSpacesState> {
+    constructor(props: ListSpacesProps) {
         super(props);
-        this.state = {spaces: []};
+        this.state = {spaceId: this.props.prop, spaces: []};
     }
 
     componentDidMount() {
         let spacesResource: SpaceResourceApi = new SpaceResourceApi(); 
-        spacesResource.getSpacesBelongingToSpaceUsingGET(-1).then(spacesResult => {
-            var state: ListSpacesState = {spaces: spacesResult.data};
+        spacesResource.getSpacesBelongingToSpaceUsingGET(this.state.spaceId).then(spacesResult => {
+            var state: ListSpacesState = {spaceId: this.state.spaceId, spaces: spacesResult.data};
             this.setState(state);
         })
+    }
+
+    onClickHandler(newSpaceId: any) {
+        this.state = {spaceId: newSpaceId, spaces: []};
+        this.componentDidMount();
     }
 
     render() {
         return (
             <div>
                 {this.state.spaces.map(space => (
-                    <div key={space.id}>id: {space.id}, name: {space.name}</div>
+                    <div key={space.id} onClick={() => this.onClickHandler(space.id)}>id: {space.id}, name: {space.name}</div>
                 ))}
             </div>
         );
     }
 }
 
+interface ListSpacesProps {
+    prop: number;
+}
+
 interface ListSpacesState {
+    spaceId: number;
     spaces: SpaceDTO[];
 }

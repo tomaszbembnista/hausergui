89bc65404c5ef617583011cf89dfa7add71384b1
Piotr Baranowski <piotr267@protonmail.com>
2020-10-20 11:38:01 +0200

    It is now possible to explore spaces

--- src/app/ListSpaces.tsx
+++ src/app/ListSpaces.tsx
@@ -1,50 +1,70 @@
 import React from "react";
 import { SpaceResourceApi } from "./srvapi";
 import { SpaceDTO } from "./srvapi/models/space-dto";
+import { forkJoin, of } from 'rxjs';
 import { Card, Typography, CardActions, Button } from "@material-ui/core";
 import ArrowBackIosOutlinedIcon from '@material-ui/icons/ArrowBackIosOutlined';
+import { AxiosResponse } from "axios";
 
 export default class ListSpaces extends React.Component<ListSpacesProps, ListSpacesState> {
     constructor(props: ListSpacesProps) {
         super(props);
         this.state = {
-            spaceId: this.props.parentSpaceId, 
+            spaceId: this.props.parentSpaceId,
             spaceName: "Spaces",
             ancestorsPath: [],
-            spaces: []};
-    }
-    
-    componentDidMount() {
-        this.getSubspaces(this.props.parentSpaceId);
+            spaces: []
+        };
     }
 
-    getSubspaces(parentSpaceId: number) {
+    componentDidMount() {
+        this.getSpaceDetails(this.props.parentSpaceId);
+    }
+
+    getSpaceDetails(parentSpaceId: number) {
         let spacesResource: SpaceResourceApi = new SpaceResourceApi();
-        let newSpaceName: string;
-        if (parentSpaceId !== -1) { 
-            spacesResource.getSpaceUsingGET(parentSpaceId).then(spaceResult => {
-                newSpaceName = spaceResult.data.name;
-            })
+
+        let defaultSpace: SpaceDTO = {
+            id: parentSpaceId,
+            name: "Spaces",
+            parentId: null,
+            slug: null
         }
-        else {
-            newSpaceName = "Spaces";
+
+        let defaultSpaceWrapper: AxiosResponse<SpaceDTO> = {
+            data: defaultSpace,
+            status: 200,
+            statusText: "",
+            headers: "",
+            config: {}
         }
-        spacesResource.getSpacesBelongingToSpaceUsingGET(parentSpaceId).then(spacesResult => {
-            let state: ListSpacesState = {
-                spaceId: parentSpaceId,
-                spaceName: newSpaceName, 
-                ancestorsPath: this.state.ancestorsPath.concat(parentSpaceId),
-                spaces: spacesResult.data
-            };
-            this.setState(state);
-        })
+
+        let spaceObservable: Promise<AxiosResponse<SpaceDTO>> = of(defaultSpaceWrapper).toPromise();
+
+        if (parentSpaceId !== -1) {
+            spaceObservable = spacesResource.getSpaceUsingGET(parentSpaceId);
+        }
+
+        forkJoin([
+            spaceObservable,
+            spacesResource.getSpacesBelongingToSpaceUsingGET(parentSpaceId)
+        ])
+            .subscribe((values) => {
+                let state: ListSpacesState = {
+                    spaceId: parentSpaceId,
+                    spaceName: values[0].data.name,
+                    ancestorsPath: this.state.ancestorsPath.concat(parentSpaceId),
+                    spaces: values[1].data
+                };
+                this.setState(state);
+            });
     }
 
     backButtonOnClickHandler() {
         if (this.state.ancestorsPath.length > 0) {
             let parentSpaceId: number = this.state.ancestorsPath.pop() as number;
             parentSpaceId = this.state.ancestorsPath.pop() as number;
-            this.getSubspaces(parentSpaceId);
+            this.getSpaceDetails(parentSpaceId);
         }
     }
 
@@ -54,14 +74,14 @@ export default class ListSpaces extends React.Component<ListSpacesProps, ListSpa
                 <Typography variant="h5" component="h2">
                     {this.state.spaceName}
                 </Typography>
-                
+
                 {this.state.spaces.map(space => (
                     <Card variant="outlined" key={space.id}>
                         <Typography variant="h5" component="h2">
                             {space.name}
                         </Typography>
                         <CardActions>
-                            <Button size="small" onClick={() => this.getSubspaces(space.id)}>Learn More</Button>
+                            <Button size="small" onClick={() => this.getSpaceDetails(space.id)}>Learn More</Button>
                         </CardActions>
                     </Card>
                 ))}

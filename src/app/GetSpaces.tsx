3eb7cf9677691a18d8a2db8f51479c1521f74b44
Piotr Baranowski <piotr267@protonmail.com>
2020-10-21 13:14:22 +0200

    Changes in rendering, GetSpaces has got render() again

--- src/app/GetSpaces.tsx
+++ src/app/GetSpaces.tsx
@@ -1,8 +1,29 @@
+import React from "react";
 import { SpaceResourceApi, SpaceDTO } from "./srvapi/index";
-import { forkJoin, of, Observable } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
+import { Card, Typography, CardActions, Button, CardHeader, CardContent } from "@material-ui/core";
+import ArrowBackIosOutlinedIcon from '@material-ui/icons/ArrowBackIosOutlined';
 
-export default class GetSpaces {
-    getSpaceDetails(spaceId: number, subspaces: SpaceDTO[]): Observable<[SpaceDTO, SpaceDTO[]]> {
+export default class GetSpaces extends React.Component<ListSpacesProps, ListSpacesState> {
+    constructor(props: ListSpacesProps) {
+        super(props);
+        this.state = {
+            spaceData: {
+                id: this.props.parentSpaceId,
+                name: "Spaces",
+                parentId: undefined,
+                slug: undefined
+            },
+            ancestorsPath: [],
+            spaces: []
+        };
+    }
+
+    componentDidMount() {
+        this.getSpaceDetails(this.props.parentSpaceId);
+    }
+
+    getSpaceDetails(spaceId: number) {
         let spacesResource: SpaceResourceApi = new SpaceResourceApi();
 
         let defaultSpace: SpaceDTO = {
@@ -14,7 +35,7 @@ export default class GetSpaces {
 
         let spaceObservable: Promise<SpaceDTO> = of(defaultSpace).toPromise();
 
-        let subspace: SpaceDTO | undefined = subspaces.find(space => {
+        let subspace: SpaceDTO | undefined = this.state.spaces.find(space => {
             return space.id === spaceId;
         });
 
@@ -25,11 +46,66 @@ export default class GetSpaces {
             spaceObservable = spacesResource.getSpaceUsingGET({ id: spaceId });
         }
 
-        let getResponse = forkJoin([
+        forkJoin([
             spaceObservable,
             spacesResource.getSpacesBelongingToSpaceUsingGET({ id: spaceId })
-        ]);
-
-        return getResponse;
+        ]).subscribe((values) => {
+            let state: ListSpacesState = {
+                spaceData: {
+                    id: spaceId,
+                    name: values[0].name,
+                    parentId: values[0].parentId,
+                    slug: values[0].slug
+                },
+                ancestorsPath: this.state.ancestorsPath.concat(spaceId),
+                spaces: values[1]
+            };
+            this.setState(state);
+        });
     }
+
+    backButtonOnClickHandler() {
+        if (this.state.ancestorsPath.length > 0) {
+            let parentSpaceId: number = this.state.ancestorsPath.pop() as number;
+            parentSpaceId = this.state.ancestorsPath.pop() as number;
+            this.getSpaceDetails(parentSpaceId);
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <CardHeader
+                    action={
+                        this.state.spaceData.id as number > -1 && <ArrowBackIosOutlinedIcon onClick={() => this.backButtonOnClickHandler()} />
+                    }
+                    title={
+                        this.state.spaceData.name
+                    }
+                />
+                <CardContent>
+                    {this.state.spaces.map(space => (
+                        <Card variant="outlined" key={space.id} className="subcard">
+                            <Typography variant="h5" component="h2">
+                                {space.name}
+                            </Typography>
+                            <CardActions>
+                                <Button size="small" onClick={() => this.getSpaceDetails(space.id as number)}>Enter</Button>
+                            </CardActions>
+                        </Card>
+                    ))}
+                </CardContent>
+            </>
+        )
+    }
+}
+
+interface ListSpacesProps {
+    parentSpaceId: number;
+}
+
+interface ListSpacesState {
+    spaceData: SpaceDTO;
+    ancestorsPath: number[];
+    spaces: SpaceDTO[];
 }
